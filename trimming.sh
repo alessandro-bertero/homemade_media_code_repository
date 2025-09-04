e7765d93d9e6321aa9662c34ea7267abd23da088
sara-bianchi <sara.bianchi447@edu.unito.it>
2025-09-04 18:03:16 +0200

    Delete trimming.sh

--- trimming.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-fastq_files=/home/ebalmas/scpHUB/projects/EB002AS1/analysis_EB_SB/scratch/CompGenomics/Data/RNA-Seq/*.fastq.gz
-for i in $fastq_files
-do
-	trim_galore --adapter CTGTCTCTTATACACATCT --trim1 --clip_R1 4 --gzip --fastqc --output_dir ./Data/Trimmed_fastq $i
-done
