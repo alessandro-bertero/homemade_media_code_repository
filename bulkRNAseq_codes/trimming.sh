509905baef24e812d97e6c2d51d3386010379875
sara-bianchi <sara.bianchi447@edu.unito.it>
2025-09-04 19:10:32 +0200

    Delete bulkRNAseq_codes directory

--- bulkRNAseq_codes/trimming.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-fastq_files=/home/shared_folder/fastq/*.fastq.gz
-for i in $fastq_files
-do
-	trim_galore --adapter CTGTCTCTTATACACATCT --trim1 --clip_R1 4 --gzip --fastqc --output_dir ./Data/Trimmed_fastq $i
-done
